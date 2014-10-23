25cea66d34ad1cb8058d9227cf8017e39e91636e
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-10-23 16:17:47 +0400

    A dual execution model

--- library/Hasql.hs
+++ library/Hasql.hs
@@ -10,7 +10,20 @@ module Hasql
   Transaction.Mode,
 
   -- ** Execution
+  -- | For your convenience there are two models of execution:
+  -- 
+  -- * A simple function on IO.
+  -- 
+  -- * A session monad transformer, 
+  -- which is an adaptation of the 'ReaderT' API over the connections pool.
+  
+  -- *** Simple IO
   txIO,
+
+  -- *** Session
+  Session,
+  sessionInner,
+  txSession,
 
   -- ** Transactions
   Transaction.StatementTx,
@@ -49,4 +62,29 @@ txIO ::
   Pool.Pool b -> Transaction.Mode -> (forall s. Transaction.Transaction b s r) -> IO r
 txIO p m t =
   Pool.withConnection (\c -> Transaction.txIO m c t) p
+
+
+-- * Session
+-------------------------
+
+-- |
+-- A convenience monad transformer, 
+-- which is just a simple wrapper around a 'ReaderT'.
+type Session b =
+  ReaderT (Pool.Pool b)
+
+-- |
+-- Run the session monad transformer in the inner monad.
+sessionInner :: Pool.Pool b -> Session b m r -> m r
+sessionInner pool reader =
+  runReaderT reader pool
+
+-- |
+-- Execute a transaction in a session.
+txSession :: 
+  Backend.Backend b => MonadIO m =>
+  Transaction.Mode -> (forall s. Transaction.Transaction b s r) -> Session b m r
+txSession m t =
+  ReaderT $ \p -> liftIO $ txIO p m t
+
 
