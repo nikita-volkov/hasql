56ab4f821cd9c1132ef493ffe4bd3ac2d521d060
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-04-13 00:19:04 +0300

    Implement buffer

--- library/Hasql/Buffer.hs
+++ library/Hasql/Buffer.hs
@@ -7,58 +7,77 @@ import Foreign.C
 foreign import ccall unsafe "memmove"
   memmove :: Ptr a {-^ Destination -} -> Ptr a {-^ Source -} -> CSize {-^ Count -} -> IO (Ptr a) {-^ Destination -}
 
+foreign import ccall unsafe "memcpy"
+  memcpy :: Ptr a {-^ Destination -} -> Ptr a {-^ Source -} -> CSize {-^ Count -} -> IO (Ptr a) {-^ Destination -}
 
-data Buffer =
-  Buffer !(ForeignPtr Word8) !(IORef State)
+
+newtype Buffer =
+  Buffer (MVar State)
 
 data State =
   {-|
+  * Buffer pointer
   * Start offset
   * End offset
   * Size
   -}
-  State !Int !Int !Int
+  State !(ForeignPtr Word8) !Int !Int !Int
 
 new :: Int -> IO Buffer
 new space =
-  Buffer <$> mallocForeignPtrBytes space <*> newIORef (State 0 0 0)
-
-getRemainingSpace :: Buffer -> IO Int
-getRemainingSpace (Buffer _ stateRef) =
   do
-    State start end size <- readIORef stateRef
-    return (size - end)
+    fptr <- mallocForeignPtrBytes space
+    stateMVar <- newMVar (State fptr 0 0 space)
+    return (Buffer stateMVar)
 
 {-|
-Ensure that the buffer has at least the specified amount of elements.
--}
-demandSpace :: Buffer -> Int -> IO ()
-demandSpace (Buffer fptr stateRef) space =
-  do
-    State start end size <- readIORef stateRef
-    let
-      remainingSpace = size - end
-      delta = space - remainingSpace
-      occupiedSpace = end - start
-      in if delta <= 0
-        then
-          return ()
-        else 
-          if delta > start
-            then do
-              -- Grow
-              traceEventIO ("START Buffer/Grow")
-              $(todo "Grow")
-            else do
-              traceEventIO ("START Buffer/Align")
-              withForeignPtr fptr (\ptr -> memmove ptr (plusPtr ptr start) (fromIntegral occupiedSpace))
-              traceEventIO ("STOP Buffer/Align")
-              writeIORef stateRef (State 0 occupiedSpace size)
+Fill the buffer with the specified amount of bytes.
 
-{-|
+Aligns or grows the buffer if required.
+
 It is the user's responsibility that the pointer action
 does not exceed the limits.
 -}
-fill :: Buffer -> (Int -> Ptr Word8 -> IO Int) -> IO ()
-fill (Buffer fptr stateRef) =
-  $(todo "")
+put :: Buffer -> Int {-^ Amount of bytes to be written -} -> (Ptr Word8 -> IO ()) {-^ Poker -} -> IO ()
+put (Buffer stateMVar) space ptrIO =
+  do
+    State fptr start end boundary <- takeMVar stateMVar
+    let
+      remainingSpace = boundary - end
+      delta = space - remainingSpace
+      occupiedSpace = end - start
+      in if delta <= 0
+        then do
+          withForeignPtr fptr $ \ptr -> do
+            ptrIO (plusPtr ptr end)
+          putMVar stateMVar (State fptr start (end + space) boundary)
+        else if delta > start
+          then do
+            -- Grow
+            let
+              newOccupiedSpace = occupiedSpace + space
+            traceEventIO ("START Buffer/Grow")
+            newFPtr <- mallocForeignPtrBytes newOccupiedSpace
+            withForeignPtr newFPtr $ \newPtr -> do
+              withForeignPtr fptr $ \ptr -> do
+                memcpy newPtr (plusPtr ptr start) (fromIntegral occupiedSpace)
+              traceEventIO ("STOP Buffer/Grow")
+              ptrIO (plusPtr newPtr occupiedSpace)
+            putMVar stateMVar (State newFPtr 0 newOccupiedSpace newOccupiedSpace)
+          else do
+            -- Align
+            withForeignPtr fptr $ \ptr -> do
+              traceEventIO ("START Buffer/Align")
+              memmove ptr (plusPtr ptr start) (fromIntegral occupiedSpace)
+              traceEventIO ("STOP Buffer/Align")
+              ptrIO (plusPtr ptr occupiedSpace)
+            putMVar stateMVar (State fptr 0 (occupiedSpace + space) boundary)
+
+take :: Buffer -> (Ptr Word8 -> Int -> IO (result, Int)) -> IO result
+take (Buffer stateMVar) ptrIO =
+  do
+    State fptr start end boundary <- takeMVar stateMVar
+    withForeignPtr fptr $ \ptr -> do
+      (result, amountTaken) <- ptrIO (plusPtr ptr start) (end - start)
+      putMVar stateMVar (State fptr (start + amountTaken) end boundary)
+      return result
