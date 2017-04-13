e41f83fbfef25cc7af46a7db47c711e96a32d0bf
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-04-13 15:38:56 +0300

    Enlarge the benchmark samples
    Results:
    
    benchmarking largeResultInVector
    time                 7.644 ms   (7.531 ms .. 7.805 ms)
                         0.999 R²   (0.998 R² .. 1.000 R²)
    mean                 7.590 ms   (7.552 ms .. 7.633 ms)
    std dev              114.4 μs   (84.40 μs .. 168.9 μs)
    
    benchmarking largeResultInList
    time                 9.688 ms   (9.597 ms .. 9.762 ms)
                         0.999 R²   (0.999 R² .. 1.000 R²)
    mean                 9.686 ms   (9.631 ms .. 9.763 ms)
    std dev              181.3 μs   (120.3 μs .. 291.4 μs)
    
    benchmarking manySmallResults
    time                 53.05 ms   (52.51 ms .. 53.65 ms)
                         1.000 R²   (0.999 R² .. 1.000 R²)
    mean                 52.71 ms   (52.46 ms .. 52.98 ms)
    std dev              468.0 μs   (315.9 μs .. 607.8 μs)

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -92,7 +92,7 @@ queryWithManyRows decoder =
   C.statement template encoder (decoder rowDecoder) True
   where
     template =
-      "SELECT generate_series(0,1000) as a, generate_series(1000,2000) as b"
+      "SELECT generate_series(0,10000) as a, generate_series(10000,20000) as b"
     encoder =
       conquer
     rowDecoder =
