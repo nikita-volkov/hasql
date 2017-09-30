997aed959a8599cb5049ce6d8177c4f1866449d8
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-09-30 13:49:01 +0300

    Core Model

---
+++ library/Hasql/Core/Model.hs
@@ -0,0 +1,50 @@
+module Hasql.Core.Model where
+
+import Hasql.Prelude hiding (State, peek)
+
+
+{-|
+An internal request to the dispatcher.
+-}
+data Request
+  -- ParseRequest !ByteString !ByteString !(Vector Word32) !(Either Error () -> IO ()) |
+  -- BindRequest !ByteString !ByteString !D.Encoding !(Either Error () -> IO ()) |
+  -- ExecuteRequest 
+
+
+data Notification = Notification !Int32 !ByteString !ByteString
+
+data Message = Message !Word8 !ByteString
+
+data Error =
+  {-| 
+  An erroneous result received from the DB.
+  The components are:
+
+  * The SQLSTATE code for the error. The SQLSTATE code identifies the type of error that has occurred; 
+  it can be used by front-end applications to perform specific operations (such as error handling) 
+  in response to a particular database error. 
+  For a list of the possible SQLSTATE codes, see Appendix A.
+  This field is not localizable, and is always present.
+
+  * The primary human-readable error message (typically one line). Always present.
+  -}
+  BackendError !ByteString !ByteString |
+  {-|
+  Can happen as a result of an incorrect decoder being applied.
+  -}
+  DecodingError !Text |
+  {-|
+  Problems with the connection.
+  -}
+  TransportError !Text |
+  {-|
+  An unexpected or broken data packet received from the server.
+  Can happen as a result of 
+  the server sending an unsupported message or
+  something interfering
+  in the communication channel.
+  This error type is highly unlikely.
+  -}
+  ProtocolError !Text
+  deriving (Show)
