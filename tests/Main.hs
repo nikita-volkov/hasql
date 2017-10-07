e52fec1200ea3ef931f5bf2445164533b4d44fdf
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-07 15:05:14 +0300

    Update the tests to manually establish the connection

--- tests/Main.hs
+++ tests/Main.hs
@@ -7,8 +7,46 @@ import Test.Tasty
 import Test.Tasty.Runners
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
-import qualified ConnectedTests as A
+import qualified Hasql.Connection as A
+import qualified Hasql.Query as J
+import qualified Data.Vector as H
+import qualified Control.Foldl as I
+import qualified Hasql.DecodeResult as B
+import qualified Hasql.DecodeRow as C
+import qualified Hasql.DecodePrimitive as D
 
 
 main =
-  defaultMain A.tests
+  connect >>= runTests
+
+connect :: IO A.Connection
+connect =
+  do
+    openingResult <- A.open (A.TCPConnectionSettings "localhost" 5432) "postgres" "" Nothing handleErrorOrNotification
+    case openingResult of
+      Left error -> fail (showString "Can't connect: " (show error))
+      Right connection -> return connection
+  where
+    handleErrorOrNotification x =
+      putStrLn ("Async event: " <> show x)
+
+runTests :: A.Connection -> IO ()
+runTests connection =
+  defaultMain $
+  testGroup "Tests with connection" $
+  [
+    testCase "Combined query of select '1' and select 'true'" $ 
+    let
+      query :: J.Query (Int64, Bool)
+      query = (,) <$> selectOne <*> selectTrue
+        where
+          selectOne = J.preparedStatement "select 1 :: int8" mempty (B.row (C.nonNullPrimitive D.int8))
+          selectTrue = J.preparedStatement "select 'true' :: bool" mempty (B.row (C.nonNullPrimitive D.bool))
+      in do
+        result <- A.query connection query
+        assertEqual "" (Right (1, True)) result
+    ,
+    testCase "Simultaneous result decoding and counting" $
+    return ()
+  ]
+
