c4422ae5eb10f8c5f0b7c6e42bee8c27710b2f52
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-09 14:28:43 +0300

    Update tests and add more

--- tests/Main.hs
+++ tests/Main.hs
@@ -46,12 +46,24 @@ runTests connection =
         testGroup "Query" $
         [
           test "select 1" (Right 1) $
-          J.preparedStatement "select 1 :: int8" mempty (B.row (C.nonNullPrimitive D.int8))
+          J.preparedStatement "select 1" mempty (B.row (C.nonNullPrimitive D.int8))
           ,
           test "select '1' and select 'true'" (Right (1, True)) $
           (,) <$>
-          J.preparedStatement "select 1 :: int8" mempty (B.row (C.nonNullPrimitive D.int8)) <*>
+          J.preparedStatement "select 1" mempty (B.row (C.nonNullPrimitive D.int8)) <*>
           J.preparedStatement "select 'true' :: bool" mempty (B.row (C.nonNullPrimitive D.bool))
+          ,
+          test "Error" (Left (E.BackendError "42703" "column \"abc\" does not exist")) $
+          J.preparedStatement "select abc" mempty (B.row (C.nonNullPrimitive D.int8))
+          ,
+          test "Errors in multiple queries" (Left (E.BackendError "42703" "column \"abc\" does not exist")) $
+          J.preparedStatement "select 1" mempty (B.row (C.nonNullPrimitive D.int8)) *>
+          J.preparedStatement "select abc" mempty (B.row (C.nonNullPrimitive D.int8)) *>
+          J.preparedStatement "select abc" mempty (B.row (C.nonNullPrimitive D.int8))
+          ,
+          test "traverse" (Right [1,2,3]) $
+          traverse (\template -> J.preparedStatement template mempty (B.row (C.nonNullPrimitive D.int8))) $
+          ["select 1", "select 2", "select 3"]
           ,
           testCaseInfo "Simultaneous result decoding and counting" $ pure "Pending"
         ]
