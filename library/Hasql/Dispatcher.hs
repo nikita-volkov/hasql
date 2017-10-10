7ea7ca05b8d09fd4ce6267efd00525921c2380ff
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-10 10:58:01 +0300

    Reimplement Dispatcher

--- library/Hasql/Dispatcher.hs
+++ library/Hasql/Dispatcher.hs
@@ -6,14 +6,11 @@ import qualified Hasql.Socket as A
 import qualified ByteString.StrictBuilder as B
 import qualified BinaryParser as D
 import qualified PtrMagic.Encoding as F
-import qualified Hasql.ParseMessageStream as E
-import qualified Hasql.ParseMessage as H
 import qualified Hasql.Request as C
 import qualified Hasql.InteractUnauthenticated as G
 import qualified Hasql.Loops.Serializer as SerializerLoop
 import qualified Hasql.Loops.Receiver as ReceiverLoop
 import qualified Hasql.Loops.Sender as SenderLoop
-import qualified Hasql.Loops.IncomingMessagesSlicer as IncomingMessagesSlicerLoop
 import qualified Hasql.Loops.Interpreter as InterpreterLoop
 
 
@@ -26,35 +23,32 @@ data Dispatcher =
 type PerformRequest =
   forall result. C.Request result -> IO (Either Error result)
 
-start :: A.Socket -> (Either Error Notification -> IO ()) -> IO Dispatcher
-start socket sendErrorOrNotification =
+start :: A.Socket -> (Notification -> IO ()) -> IO Dispatcher
+start socket sendNotification =
   do
     outgoingBytesQueue <- newTQueueIO
-    incomingBytesQueue <- newTQueueIO
     serializerMessageQueue <- newTQueueIO
-    incomingMessageQueue <- newTQueueIO
+    responseQueue <- newTQueueIO
     resultProcessorQueue <- newTQueueIO
     transportErrorVar <- newEmptyTMVarIO
     let
       performRequest :: PerformRequest
-      performRequest (C.Request builder parseExcept) =
+      performRequest (C.Request builder ir) =
         do
           resultVar <- newEmptyTMVarIO
           atomically $ do
-            writeTQueue resultProcessorQueue (InterpreterLoop.ResultProcessor parse (sendResult resultVar))
+            writeTQueue resultProcessorQueue (InterpreterLoop.ResultProcessor ir (sendResult resultVar))
             writeTQueue serializerMessageQueue (SerializerLoop.SerializeMessage encoding)
             writeTQueue serializerMessageQueue (SerializerLoop.FlushMessage)
           atomically (takeTMVar resultVar)
         where
           encoding =
             B.builderPtrFiller builder F.Encoding
-          parse =
-            fmap (either (\(C.BackendError state details) -> Left (BackendError state details)) Right) (runExceptT parseExcept)
-          sendResult resultVar resultOrError =
+          sendResult resultVar errorOrResult =
             do
-              result <- case resultOrError of
-                Left message -> return (Left (DecodingError message))
-                Right result -> atomically (fmap (Left . TransportError) (readTMVar transportErrorVar) <|> pure result)
+              result <- case errorOrResult of
+                Left error -> return (Left error)
+                Right result -> atomically (fmap (Left . TransportError) (readTMVar transportErrorVar) <|> pure (Right result))
               atomically (putTMVar resultVar result)
       loopSending =
         SenderLoop.loop socket
@@ -62,29 +56,19 @@ start socket sendErrorOrNotification =
           (atomically . putTMVar transportErrorVar)
       loopReceiving =
         ReceiverLoop.loop socket
-          (atomically . writeTQueue incomingBytesQueue)
+          (atomically . writeTQueue responseQueue)
           (atomically . putTMVar transportErrorVar)
       loopSerializing =
         SerializerLoop.loop
           (atomically (readTQueue serializerMessageQueue))
           (atomically . writeTQueue outgoingBytesQueue)
-      loopSlicing =
-        IncomingMessagesSlicerLoop.loop
-          (atomically (readTQueue incomingBytesQueue))
-          (atomically . writeTQueue incomingMessageQueue)
       loopInterpreting =
         InterpreterLoop.loop
-          (atomically (readTQueue incomingMessageQueue))
+          (atomically (readTQueue responseQueue))
           (atomically (tryReadTQueue resultProcessorQueue))
-          (\case
-            InterpreterLoop.NotificationUnaffiliatedResult notification ->
-              sendErrorOrNotification (Right notification)
-            InterpreterLoop.ErrorMessageUnaffiliatedResult (ErrorMessage state details) ->
-              sendErrorOrNotification (Left (BackendError state details))
-            InterpreterLoop.ProtocolErrorUnaffiliatedResult details ->
-              sendErrorOrNotification (Left (ProtocolError details)))
+          (sendNotification)
       in do
-        kill <- startThreads [loopInterpreting, loopSerializing, loopSlicing, loopSending, loopReceiving]
+        kill <- startThreads [loopInterpreting, loopSerializing, loopSending, loopReceiving]
         return (Dispatcher kill performRequest)
 
 interact :: Dispatcher -> G.Interact result -> IO (Either Error result)
