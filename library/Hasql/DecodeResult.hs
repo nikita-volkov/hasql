0c8879714f33b3accdc0f92863495dc4c079447d
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-10 11:22:20 +0300

    Reimplement DecodeResult

--- library/Hasql/DecodeResult.hs
+++ library/Hasql/DecodeResult.hs
@@ -2,28 +2,28 @@ module Hasql.DecodeResult where
 
 import Hasql.Prelude
 import Hasql.Model
-import qualified Hasql.ParseMessageStream as A
+import qualified Hasql.InterpretResponses as A
 import qualified Hasql.DecodeRow as B
 
 
 newtype DecodeResult result =
-  DecodeResult (ReaderT Bool A.ParseMessageStream result)
+  DecodeResult (ReaderT Bool A.InterpretResponses result)
   deriving (Functor)
 
 rowsAffected :: DecodeResult Int
 rowsAffected =
   DecodeResult (ReaderT (const (A.rowsAffected)))
 
-rows :: Fold row result -> B.DecodeRow row -> DecodeResult result
+rows :: FoldM IO row result -> B.DecodeRow row -> DecodeResult (result, Int)
 rows fold (B.DecodeRow (ReaderT parseDataRow)) =
-  DecodeResult (ReaderT (\idt -> A.rows (parseDataRow idt) fold))
+  DecodeResult (ReaderT (\idt -> A.foldRows fold (parseDataRow idt)))
 
 {-|
 Exactly one row
 -}
 row :: B.DecodeRow row -> DecodeResult row
 row (B.DecodeRow (ReaderT parseDataRow)) =
-  DecodeResult (ReaderT (\idt -> A.row (parseDataRow idt)))
+  DecodeResult (ReaderT (\idt -> A.singleRow (parseDataRow idt)))
 
 unit :: DecodeResult ()
 unit =
