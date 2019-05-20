68339eeabefdf10be0ac47b840d296ae71f36bc2
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-05-20 19:27:10 +0300

    Ditch the coersion of encoders

--- library/Hasql/Private/Session.hs
+++ library/Hasql/Private/Session.hs
@@ -7,6 +7,7 @@ import qualified Database.PostgreSQL.LibPQ as LibPQ
 import qualified Hasql.Private.Decoders.Results as Decoders.Results
 import qualified Hasql.Private.Decoders.Result as Decoders.Result
 import qualified Hasql.Private.Encoders.Params as Encoders.Params
+import qualified Hasql.Private.Encoders as Encoders
 import qualified Hasql.Private.Settings as Settings
 import qualified Hasql.Private.IO as IO
 import qualified Hasql.Statement as Statement
@@ -44,16 +45,16 @@ sql sql =
 -- |
 -- Parameters and a specification of a parametric single-statement query to apply them to.
 statement :: params -> Statement.Statement params result -> Session result
-statement input (Statement.Statement template encoder decoder preparable) =
+statement input (Statement.Statement template (Encoders.Params paramsEncoder) decoder preparable) =
   Session $ ReaderT $ \(Connection.Connection pqConnectionRef integerDatetimes registry) ->
     ExceptT $ fmap (mapLeft (QueryError template inputReps)) $ withMVar pqConnectionRef $ \pqConnection -> do
-      r1 <- IO.sendParametricStatement pqConnection integerDatetimes registry template (unsafeCoerce encoder) preparable input
+      r1 <- IO.sendParametricStatement pqConnection integerDatetimes registry template paramsEncoder preparable input
       r2 <- IO.getResults pqConnection integerDatetimes (unsafeCoerce decoder)
       return $ r1 *> r2
   where
     inputReps =
       let
-        Encoders.Params.Params (Op encoderOp) = (unsafeCoerce encoder)
+        Encoders.Params.Params (Op encoderOp) = paramsEncoder
         step (_, _, _, rendering) acc =
           rendering : acc
         in foldr step [] (encoderOp input)
