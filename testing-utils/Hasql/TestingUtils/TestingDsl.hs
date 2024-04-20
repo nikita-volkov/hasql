b49c94ed2a2b81a3f134ec44984555cbda38a02a
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-04-20 13:59:11 +0300

    Factor constants out

--- testing-utils/Hasql/TestingUtils/TestingDsl.hs
+++ testing-utils/Hasql/TestingUtils/TestingDsl.hs
@@ -1,4 +1,4 @@
-module Hasql.TestingUtils.Session
+module Hasql.TestingUtils.TestingDsl
   ( Session.Session,
     SessionError (..),
     Session.QueryError (..),
@@ -11,6 +11,7 @@ where
 import Hasql.Connection qualified as Connection
 import Hasql.Session qualified as Session
 import Hasql.Statement qualified as Statement
+import Hasql.TestingUtils.Constants qualified as Constants
 import Prelude
 
 data SessionError
@@ -23,16 +24,7 @@ runSession session =
   runExceptT $ acquire >>= \connection -> use connection <* release connection
   where
     acquire =
-      ExceptT $ fmap (mapLeft ConnectionError) $ Connection.acquire settings
-      where
-        settings =
-          Connection.settings host port user password database
-          where
-            host = "localhost"
-            port = 5432
-            user = "postgres"
-            password = "postgres"
-            database = "postgres"
+      ExceptT $ fmap (mapLeft ConnectionError) $ Connection.acquire Constants.localConnectionSettings
     use connection =
       ExceptT
         $ fmap (mapLeft SessionError)
