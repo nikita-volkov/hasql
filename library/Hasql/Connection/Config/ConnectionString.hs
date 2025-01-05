9444102080000da78de95bbce11912bb3be89ca7
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-01-05 23:10:07 +0300

    Add construction from text

--- library/Hasql/Connection/Config/ConnectionString.hs
+++ library/Hasql/Connection/Config/ConnectionString.hs
@@ -2,6 +2,7 @@ module Hasql.Connection.Config.ConnectionString where
 
 import Data.ByteString qualified as B
 import Data.Map.Strict qualified as Map
+import Data.Text.Encoding qualified
 import Hasql.Connection.Config.ConnectionString.Params qualified as Params
 import Hasql.Prelude
 
@@ -9,6 +10,9 @@ type ConnectionString = ByteString
 
 class Constructs a where
   construct :: a -> ConnectionString
+
+fromText :: Text -> ConnectionString
+fromText = Data.Text.Encoding.encodeUtf8
 
 fromParams :: Params.Params -> ConnectionString
 fromParams =
