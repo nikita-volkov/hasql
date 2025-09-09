8fbaadf92c0c2171a23d6a75e704a5f32d47476f
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-09-09 23:29:04 +0300

    Polish

--- library/Hasql/PreparedStatementRegistry/Map.hs
+++ library/Hasql/PreparedStatementRegistry/Map.hs
@@ -12,22 +12,22 @@ module Hasql.PreparedStatementRegistry.Map
 where
 
 import ByteString.StrictBuilder qualified as B
-import Data.HashMap.Strict qualified as HM
+import Data.HashMap.Strict qualified as HashMap
 import Hasql.LibPq14 qualified as Pq
 import Hasql.Prelude hiding (empty, insert, lookup, reset)
 
 -- | Pure registry state containing the hash map and counter
-data RegistryState = RegistryState !(HM.HashMap LocalKey ByteString) !Word
+data RegistryState = RegistryState (HashMap.HashMap LocalKey ByteString) Word
 
 -- | Create an empty registry state
 {-# INLINEABLE empty #-}
 empty :: RegistryState
-empty = RegistryState HM.empty 0
+empty = RegistryState HashMap.empty 0
 
 -- | Pure lookup operation
 {-# INLINEABLE lookup #-}
 lookup :: LocalKey -> RegistryState -> Maybe ByteString
-lookup localKey (RegistryState hashMap _) = HM.lookup localKey hashMap
+lookup localKey (RegistryState hashMap _) = HashMap.lookup localKey hashMap
 
 -- | Pure insert operation that returns new state and the generated remote key
 {-# INLINEABLE insert #-}
@@ -35,14 +35,14 @@ insert :: LocalKey -> RegistryState -> (ByteString, RegistryState)
 insert localKey (RegistryState hashMap counter) = (remoteKey, newState)
   where
     remoteKey = B.builderBytes . B.asciiIntegral $ counter
-    newHashMap = HM.insert localKey remoteKey hashMap
+    newHashMap = HashMap.insert localKey remoteKey hashMap
     newCounter = succ counter
     newState = RegistryState newHashMap newCounter
 
 -- | Pure reset operation
 {-# INLINEABLE reset #-}
 reset :: RegistryState -> RegistryState
-reset _ = RegistryState HM.empty 0
+reset _ = RegistryState HashMap.empty 0
 
 -- |
 -- Local statement key.
