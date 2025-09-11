0b7a54a86d9e51cb4b3b0a9ea05c7dc4503abb1b
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-09-11 21:42:33 +0300

    Reimplement Row as plain function

--- library/Hasql/Decoders/Row.hs
+++ library/Hasql/Decoders/Row.hs
@@ -13,9 +13,8 @@ import Hasql.LibPq14 qualified as Pq
 import Hasql.Prelude hiding (error)
 import PostgreSQL.Binary.Decoding qualified as A
 
-newtype Row a
-  = Row (ReaderT Env (ExceptT RowError IO) a)
-  deriving (Functor, Applicative, Monad)
+newtype Row a = Row (Env -> IO (Either RowError a))
+  deriving (Functor, Applicative, Monad) via (ReaderT Env (ExceptT RowError IO))
 
 instance MonadFail Row where
   fail = error . ValueError . fromString
@@ -27,20 +26,19 @@ data Env
 
 {-# INLINE run #-}
 run :: Row a -> (Pq.Result, Pq.Row, Pq.Column, Bool) -> IO (Either (Int, RowError) a)
-run (Row impl) (result, row, columnsAmount, integerDatetimes) =
-  do
-    columnRef <- newIORef 0
-    runExceptT (runReaderT impl (Env result row columnsAmount integerDatetimes columnRef)) >>= \case
-      Left e -> do
-        Pq.Col col <- readIORef columnRef
-        -- -1 because succ is applied before the error is returned
-        pure $ Left (fromIntegral col - 1, e)
-      Right x -> pure $ Right x
+run (Row f) (result, row, columnsAmount, integerDatetimes) = do
+  columnRef <- newIORef 0
+  let env = Env result row columnsAmount integerDatetimes columnRef
+  f env >>= \case
+    Left e -> do
+      Pq.Col col <- readIORef columnRef
+      -- -1 because succ is applied before the error is returned
+      pure $ Left (fromIntegral col - 1, e)
+    Right x -> pure $ Right x
 
 {-# INLINE error #-}
 error :: RowError -> Row a
-error x =
-  Row (ReaderT (const (ExceptT (pure (Left x)))))
+error x = Row (const (pure (Left x)))
 
 -- |
 -- Next value, decoded using the provided value decoder.
@@ -48,22 +46,19 @@ error x =
 value :: Value.Value a -> Row (Maybe a)
 value valueDec =
   {-# SCC "value" #-}
-  Row
-    $ ReaderT
-    $ \(Env result row columnsAmount integerDatetimes columnRef) -> ExceptT $ do
+  Row \(Env result row columnsAmount integerDatetimes columnRef) ->
+    do
       col <- readIORef columnRef
       writeIORef columnRef (succ col)
       if col < columnsAmount
         then do
           valueMaybe <- {-# SCC "getvalue'" #-} Pq.getvalue' result row col
-          pure
-            $ case valueMaybe of
-              Nothing ->
-                Right Nothing
-              Just value ->
-                fmap Just
-                  $ first ValueError
-                  $ {-# SCC "decode" #-} A.valueParser (Value.run valueDec integerDatetimes) value
+          pure case valueMaybe of
+            Nothing -> Right Nothing
+            Just value ->
+              fmap Just
+                $ first ValueError
+                $ {-# SCC "decode" #-} A.valueParser (Value.run valueDec integerDatetimes) value
         else pure (Left EndOfInput)
 
 -- |
