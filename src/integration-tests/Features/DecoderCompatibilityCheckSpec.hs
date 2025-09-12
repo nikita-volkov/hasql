a7f06c7a3e41914c82e6cb92bc752fe235309028
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-09-12 19:39:46 +0300

    Add tests for Pipeline

--- src/integration-tests/Features/DecoderCompatibilityCheckSpec.hs
+++ src/integration-tests/Features/DecoderCompatibilityCheckSpec.hs
@@ -3,6 +3,7 @@ module Features.DecoderCompatibilityCheckSpec (spec) where
 import Data.Either
 import Hasql.Connection qualified as Connection
 import Hasql.Decoders qualified as Decoders
+import Hasql.Pipeline qualified as Pipeline
 import Hasql.Session qualified as Session
 import Hasql.Statement qualified as Statement
 import Test.Hspec
@@ -10,8 +11,8 @@ import TestingKit.Testcontainers qualified as Testcontainers
 import Prelude
 
 spec :: Spec
-spec = aroundAll Testcontainers.withConnection do
-  describe "Error Handling" do
+spec = around Testcontainers.withConnection $ parallel do
+  describe "Session" do
     it "reports UnexpectedAmountOfColumns when result has more columns" \connection -> do
       let statement =
             Statement.Statement
@@ -64,6 +65,69 @@ spec = aroundAll Testcontainers.withConnection do
               )
               True
       result <- Connection.use connection (Session.statement () statement)
+      case result of
+        Left (Session.QueryError _ _ (Session.ResultError (Session.DecoderTypeMismatch column expected actual))) -> do
+          shouldBe column 1
+          shouldBe expected 20
+          shouldBe actual 25
+        Left err ->
+          expectationFailure ("Unexpected type of error: " <> show err)
+        result ->
+          expectationFailure ("Not an error: " <> show result)
+
+  describe "Pipeline" do
+    it "reports UnexpectedAmountOfColumns when result has more columns" \connection -> do
+      let statement =
+            Statement.Statement
+              "select 1, 2"
+              mempty
+              (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int8)))
+              True
+      result <- Connection.use connection (Session.pipeline (Pipeline.statement () statement))
+      case result of
+        Left (Session.QueryError _ _ (Session.ResultError (Session.UnexpectedAmountOfColumns expected actual))) -> do
+          shouldBe expected 1
+          shouldBe actual 2
+        Left err ->
+          expectationFailure ("Unexpected type of error: " <> show err)
+        result ->
+          expectationFailure ("Not an error: " <> show result)
+
+    it "reports UnexpectedAmountOfColumns when result has fewer columns" \connection -> do
+      let statement =
+            Statement.Statement
+              "select 1"
+              mempty
+              ( Decoders.singleRow
+                  ( (,)
+                      <$> Decoders.column (Decoders.nonNullable Decoders.int8)
+                      <*> Decoders.column (Decoders.nonNullable Decoders.int8)
+                  )
+              )
+              True
+      result <- Connection.use connection (Session.pipeline (Pipeline.statement () statement))
+      case result of
+        Left (Session.QueryError _ _ (Session.ResultError (Session.UnexpectedAmountOfColumns expected actual))) -> do
+          shouldBe expected 2
+          shouldBe actual 1
+        Left err ->
+          expectationFailure ("Unexpected type of error: " <> show err)
+        result ->
+          expectationFailure ("Not an error: " <> show result)
+
+    it "reports DecoderTypeMismatch when column type mismatches decoder" \connection -> do
+      let statement =
+            Statement.Statement
+              "select 1, 'text'"
+              mempty
+              ( Decoders.singleRow
+                  ( (,)
+                      <$> Decoders.column (Decoders.nonNullable Decoders.int8)
+                      <*> Decoders.column (Decoders.nonNullable Decoders.int8)
+                  )
+              )
+              True
+      result <- Connection.use connection (Session.pipeline (Pipeline.statement () statement))
       case result of
         Left (Session.QueryError _ _ (Session.ResultError (Session.DecoderTypeMismatch column expected actual))) -> do
           shouldBe column 1
