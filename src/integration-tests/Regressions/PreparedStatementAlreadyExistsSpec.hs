ea45f3de32c40df09258ee5b1b985f84c1df5d99
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-09-13 09:08:35 +0300

    Add a test for pipeline

--- src/integration-tests/Regressions/PreparedStatementAlreadyExistsSpec.hs
+++ src/integration-tests/Regressions/PreparedStatementAlreadyExistsSpec.hs
@@ -3,6 +3,7 @@ module Regressions.PreparedStatementAlreadyExistsSpec (spec) where
 import Data.Either
 import Hasql.Connection qualified as Connection
 import Hasql.Decoders qualified as Decoders
+import Hasql.Pipeline qualified as Pipeline
 import Hasql.Session qualified as Session
 import Hasql.Statement qualified as Statement
 import Test.Hspec
@@ -11,31 +12,64 @@ import Prelude
 
 spec :: Spec
 spec = around Testcontainers.withConnection do
-  it "Failing statements don't cause misses in updates of the prepared statement cache" \connection -> do
-    -- Run an intentionally failing prepared statement to set the condition of the bug.
-    result <- Connection.use connection do
-      Session.statement
-        ()
-        ( Statement.Statement
-            "select null"
-            mempty
-            (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
-            True
-        )
-    shouldBe (isLeft result) True
-    -- Run a succeeding prepared statement to see if the cache is still in a good state.
-    result <- Connection.use connection do
-      Session.statement
-        ()
-        ( Statement.Statement
-            "select 1"
-            mempty
-            (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
-            True
-        )
-    -- If there is an error the cache got corrupted.
-    case result of
-      Right _ ->
-        pure ()
-      Left result ->
-        expectationFailure ("Unexpected error: " <> show result)
+  describe "Session" do
+    it "Failing statements don't cause misses in updates of the prepared statement cache" \connection -> do
+      -- Run an intentionally failing prepared statement to set the condition of the bug.
+      result <- Connection.use connection do
+        Session.statement
+          ()
+          ( Statement.Statement
+              "select null"
+              mempty
+              (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+              True
+          )
+      shouldBe (isLeft result) True
+      -- Run a succeeding prepared statement to see if the cache is still in a good state.
+      result <- Connection.use connection do
+        Session.statement
+          ()
+          ( Statement.Statement
+              "select 1"
+              mempty
+              (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+              True
+          )
+      -- If there is an error the cache got corrupted.
+      case result of
+        Right _ ->
+          pure ()
+        Left result ->
+          expectationFailure ("Unexpected error: " <> show result)
+
+  describe "Pipeline" do
+    it "Failing pipeline statements don't cause misses in updates of the prepared statement cache" \connection -> do
+      -- Run an intentionally failing prepared statement in a pipeline to set the condition of the bug.
+      result <- Connection.use connection do
+        Session.pipeline do
+          Pipeline.statement
+            ()
+            ( Statement.Statement
+                "select null"
+                mempty
+                (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+                True
+            )
+      shouldBe (isLeft result) True
+      -- Run a succeeding prepared statement in a pipeline to see if the cache is still in a good state.
+      result <- Connection.use connection do
+        Session.pipeline do
+          Pipeline.statement
+            ()
+            ( Statement.Statement
+                "select 1"
+                mempty
+                (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+                True
+            )
+      -- If there is an error the cache got corrupted.
+      case result of
+        Right _ ->
+          pure ()
+        Left result ->
+          expectationFailure ("Unexpected error: " <> show result)
