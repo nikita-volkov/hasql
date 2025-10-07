42e1292d8ef7866deaa34d7067376d8b2d91d7c8
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-08 01:23:43 +0300

    Reproduce the initialization loss

---
+++ src/integration-tests/Sharing/Functionality/SessionSpec.hs
@@ -0,0 +1,38 @@
+module Sharing.Functionality.SessionSpec (spec) where
+
+import Data.Either
+import Hasql.Connection qualified as Connection
+import Helpers.Frameworks.Execution qualified as Execution
+import Helpers.NewStatements qualified as Statements
+import Helpers.Scripts qualified as Scripts
+import Test.Hspec
+import Prelude
+
+spec :: SpecWith (Text, Word16)
+spec = do
+  it "Does not lose the server-side session state on timeout" \config -> do
+    Scripts.onConnection config \connection -> do
+      varname <- Execution.generateVarname
+      result <- timeout 50_000 do
+        Connection.use connection do
+          Execution.sessionByParams (Statements.SetConfig varname "1" False)
+          Execution.sessionByParams (Statements.Sleep 0.1)
+
+      result `shouldBe` Nothing
+
+      result <- Connection.use connection do
+        Execution.sessionByParams (Statements.CurrentSetting varname True)
+
+      result `shouldBe` Right (Just "1")
+
+  it "Does not lose the server-side session state between uses" \config -> do
+    Scripts.onConnection config \connection -> do
+      varname <- Execution.generateVarname
+
+      result <- Connection.use connection do
+        Execution.sessionByParams (Statements.SetConfig varname "1" False)
+      result `shouldSatisfy` isRight
+
+      result <- Connection.use connection do
+        Execution.sessionByParams (Statements.CurrentSetting varname True)
+      result `shouldBe` Right (Just "1")
