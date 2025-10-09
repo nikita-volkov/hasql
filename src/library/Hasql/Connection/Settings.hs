cc79c23f5e0cb12eb3be35dda6a13be0239db2d4
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-09 22:14:43 +0300

    Add new settings

---
+++ src/library/Hasql/Connection/Settings.hs
@@ -0,0 +1,110 @@
+module Hasql.Connection.Settings
+  ( -- * Settings
+    Settings,
+
+    -- * Constructors
+    connectionString,
+    noPreparedStatements,
+    hosts,
+    user,
+    password,
+    dbname,
+    applicationName,
+    other,
+  )
+where
+
+import ConnectionString qualified
+import Data.Text qualified as Text
+import Platform.Prelude
+
+-- | Connection settings.
+-- This is a monoid, so you can combine multiple settings using 'mappend' ('<>').
+-- The rightmost setting takes precedence in case of conflicts.
+data Settings
+  = Settings
+      -- | Connection string.
+      ConnectionString.ConnectionString
+      -- | Disable the use of prepared statements.
+      Bool
+  deriving stock (Eq, Ord, Generic)
+  deriving anyclass (Hashable)
+
+instance Semigroup Settings where
+  Settings connectionString1 _noPreparedStatements1 <> Settings connectionString2 noPreparedStatements2 =
+    Settings
+      (connectionString1 <> connectionString2)
+      noPreparedStatements2
+
+instance Monoid Settings where
+  mempty = Settings mempty False
+
+instance Show Settings where
+  showsPrec d = showsPrec d . toConnectionString
+
+instance IsString Settings where
+  fromString = fromConnectionString . fromString
+
+toConnectionString :: Settings -> ConnectionString.ConnectionString
+toConnectionString (Settings connectionString noPreparedStatements) =
+  mconcat
+    [ connectionString,
+      if noPreparedStatements
+        then ConnectionString.param "no_prepared_statements" "true"
+        else mempty
+    ]
+
+fromConnectionString :: ConnectionString.ConnectionString -> Settings
+fromConnectionString connectionString =
+  case ConnectionString.interceptParam "no_prepared_statements" connectionString of
+    Just (value, connectionString') ->
+      let noPreparedStatements = case Text.toLower value of
+            "1" -> True
+            "true" -> True
+            "t" -> True
+            "yes" -> True
+            "y" -> True
+            "on" -> True
+            _ -> False
+       in Settings connectionString' noPreparedStatements
+    Nothing -> Settings connectionString False
+
+-- * Constructors
+
+-- | Disable the use of prepared statements.
+noPreparedStatements :: Bool -> Settings
+noPreparedStatements = Settings mempty
+
+-- | Hosts (and optional ports).
+--
+-- If multiple hosts are specified, they will be tried in order until a connection is successfully established.
+hosts :: [(Text, Maybe Word16)] -> Settings
+hosts = fromConnectionString . foldMap (uncurry ConnectionString.hostAndPort)
+
+-- | User name.
+user :: Text -> Settings
+user = fromConnectionString . ConnectionString.user
+
+-- | Password.
+password :: Text -> Settings
+password = fromConnectionString . ConnectionString.password
+
+-- | Database name.
+dbname :: Text -> Settings
+dbname = fromConnectionString . ConnectionString.dbname
+
+-- | Application name.
+applicationName :: Text -> Settings
+applicationName = fromConnectionString . ConnectionString.param "application_name"
+
+-- | Other param.
+other :: Text -> Text -> Settings
+other key = fromConnectionString . ConnectionString.param key
+
+-- | Construct from a connection string in either URI or key-value format.
+--
+-- See the [PostgreSQL documentation](https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING) for details on the format.
+--
+-- If the connection string is invalid, it will be treated as empty.
+connectionString :: Text -> Settings
+connectionString = fromConnectionString . either (const mempty) id . ConnectionString.parseText
