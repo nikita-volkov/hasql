2703c4cbeb43f394130b13003c01052e33857bc6
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-10 14:22:02 +0300

    Delegate the prepared statements flag interpretation to the stage of conversion to Config
    Ensuring to avoid multiple places where this flag can be defined and getting seamless integration with stringy values.

--- src/library/Hasql/Connection/Settings.hs
+++ src/library/Hasql/Connection/Settings.hs
@@ -23,72 +23,40 @@ import PostgresqlConnectionString qualified as ConnectionString
 -- | Connection settings.
 -- This is a monoid, so you can combine multiple settings using 'mappend' ('<>').
 -- The rightmost setting takes precedence in case of conflicts.
-data Settings
-  = Settings
-      -- | Connection string.
-      ConnectionString.ConnectionString
-      -- | Disable the use of prepared statements.
-      Bool
-  deriving stock (Eq)
+newtype Settings
+  = Settings ConnectionString.ConnectionString
+  deriving newtype (Eq, IsString, Show, Semigroup, Monoid)
 
 -- | This instance allows us to interface with the 'Config.Config' type which is internal to the library and do so without affecting the public API or constructing messy hierarchies of modules to circumvent that.
 instance Config.Constructs Settings where
-  construct (Settings connectionString noPreparedStatements) =
-    Config.Config
-      { connectionString =
-          encodeUtf8 (ConnectionString.toUrl connectionString),
-        noPreparedStatements
-      }
+  construct (Settings connectionString) =
+    case ConnectionString.interceptParam "no_prepared_statements" connectionString of
+      Just (value, connectionString) ->
+        let noPreparedStatements = interpretTextAsBool value
+         in pack connectionString noPreparedStatements
+      Nothing -> pack connectionString False
+    where
+      interpretTextAsBool value = case Text.toLower value of
+        "1" -> True
+        "true" -> True
+        "t" -> True
+        "yes" -> True
+        "y" -> True
+        "on" -> True
+        _ -> False
 
--- | Combines settings, with the rightmost taking precedence in case of conflicts.
---
--- In case of host addresses, they get combined as alternatives.
-instance Semigroup Settings where
-  Settings connectionString1 _noPreparedStatements1 <> Settings connectionString2 noPreparedStatements2 =
-    Settings
-      (connectionString1 <> connectionString2)
-      noPreparedStatements2
-
--- | Provides default settings.
-instance Monoid Settings where
-  mempty = Settings mempty False
-
--- | Renders as a string literal with a URL.
-instance Show Settings where
-  showsPrec d = showsPrec d . toConnectionString
-
--- | Constructs from a connection string in either URI or key-value format.
-instance IsString Settings where
-  fromString = fromConnectionString . fromString
-
-toConnectionString :: Settings -> ConnectionString.ConnectionString
-toConnectionString (Settings connectionString noPreparedStatements) =
-  mconcat
-    [ connectionString,
-      if noPreparedStatements
-        then ConnectionString.param "no_prepared_statements" "true"
-        else mempty
-    ]
-
-fromConnectionString :: ConnectionString.ConnectionString -> Settings
-fromConnectionString connectionString =
-  case ConnectionString.interceptParam "no_prepared_statements" connectionString of
-    Just (value, connectionString') ->
-      let noPreparedStatements = case Text.toLower value of
-            "1" -> True
-            "true" -> True
-            "t" -> True
-            "yes" -> True
-            "y" -> True
-            "on" -> True
-            _ -> False
-       in Settings connectionString' noPreparedStatements
-    Nothing -> Settings connectionString False
+      pack connectionString noPreparedStatements =
+        Config.Config
+          { connectionString =
+              let textUrl = ConnectionString.toUrl connectionString
+               in encodeUtf8 textUrl,
+            noPreparedStatements
+          }
 
 -- * Constructors
 
 -- | Whether prepared statements are disabled.
--- 
+--
 -- 'False' by default.
 --
 -- When 'True', even the statements marked as preparable will be executed without preparation at the cost of reduced performance.
@@ -97,41 +65,42 @@ fromConnectionString connectionString =
 -- Consult their docs or just provide this setting to stay on the safe side.
 -- It should be noted that starting from version @1.21.0@ @pgbouncer@ now does provide support for prepared statements.
 noPreparedStatements :: Bool -> Settings
-noPreparedStatements = Settings mempty
+noPreparedStatements =
+  Settings . ConnectionString.param "no_prepared_statements" . bool "false" "true"
 
 -- | Host domain name or IP-address.
 --
 -- To specify multiple alternate hosts, combine the produced settings via 'Monoid'.
 host :: Text -> Settings
 host host =
-  fromConnectionString (ConnectionString.host host)
+  Settings (ConnectionString.host host)
 
 -- | Host domain name or IP-address.
 --
 -- To specify multiple alternate hosts, combine the produced settings via 'Monoid'.
 hostAndPort :: Text -> Word16 -> Settings
 hostAndPort host port =
-  fromConnectionString (ConnectionString.hostAndPort host port)
+  Settings (ConnectionString.hostAndPort host port)
 
 -- | User name.
 user :: Text -> Settings
-user = fromConnectionString . ConnectionString.user
+user = Settings . ConnectionString.user
 
 -- | Password.
 password :: Text -> Settings
-password = fromConnectionString . ConnectionString.password
+password = Settings . ConnectionString.password
 
 -- | Database name.
 dbname :: Text -> Settings
-dbname = fromConnectionString . ConnectionString.dbname
+dbname = Settings . ConnectionString.dbname
 
 -- | Application name.
 applicationName :: Text -> Settings
-applicationName = fromConnectionString . ConnectionString.param "application_name"
+applicationName = Settings . ConnectionString.param "application_name"
 
 -- | Other param.
 other :: Text -> Text -> Settings
-other key = fromConnectionString . ConnectionString.param key
+other key = Settings . ConnectionString.param key
 
 -- | Construct from a connection string in either URI or key-value format.
 --
@@ -139,4 +108,4 @@ other key = fromConnectionString . ConnectionString.param key
 --
 -- If the connection string is invalid, it will be treated as empty.
 connectionString :: Text -> Settings
-connectionString = fromConnectionString . either (const mempty) id . ConnectionString.parse
+connectionString = Settings . either (const mempty) id . ConnectionString.parse
