c4a095bbcde196d67e4d3384c422ebec02a73cb6
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-21 20:16:58 +0300

    Simplify

--- src/platform/Platform/LookingUp.hs
+++ src/platform/Platform/LookingUp.hs
@@ -1,22 +1,20 @@
 module Platform.LookingUp where
 
-import Data.HashMap.Strict qualified as HashMap
-import Data.HashSet qualified as HashSet
 import Platform.Prelude
 
 data LookingUp k v f a
   = LookingUp
-      -- | Keys to populate in the provided map.
-      (HashSet k)
-      -- | Continuation that looks up values by keys in the provided map.
-      (HashMap k v -> f a)
+      -- | Keys requested to be available for lookup.
+      [k]
+      -- | Continuation that looks up values by keys.
+      ((k -> v) -> f a)
 
 deriving instance (Functor f) => Functor (LookingUp k v f)
 
-instance (Applicative f, Hashable k) => Applicative (LookingUp k v f) where
+instance (Applicative f) => Applicative (LookingUp k v f) where
   pure a =
-    LookingUp HashSet.empty (\_ -> pure a)
+    LookingUp [] (\_ -> pure a)
   LookingUp lKeys lCont <*> LookingUp rKeys rCont =
     LookingUp
       (lKeys <> rKeys)
-      (\map -> lCont map <*> rCont map)
+      (\lookup -> lCont lookup <*> rCont lookup)
