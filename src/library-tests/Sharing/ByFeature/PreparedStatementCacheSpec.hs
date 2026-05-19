6795505a40ebf42d76f697bc8fafc48299980d5f
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-05-19 12:28:00 +0300

    Add more tests

--- src/library-tests/Sharing/ByFeature/PreparedStatementCacheSpec.hs
+++ src/library-tests/Sharing/ByFeature/PreparedStatementCacheSpec.hs
@@ -123,3 +123,80 @@ spec = do
             expectationFailure ("Unexpected error on second run: " <> show other)
           Right _ ->
             expectationFailure "Second run unexpectedly succeeded"
+
+    it "A pipeline with successful statements followed by a broken one can be retried without 'already exists' errors" \config -> do
+      Scripts.onPreparableConnection config \connection -> do
+        let ok1 = Statement.preparable "select 1" mempty (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+            ok2 = Statement.preparable "select 2" mempty (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+            broken = Statement.preparable "S" mempty Decoders.noResult
+        -- First run: pipeline with two OK statements and a broken one at the end.
+        result1 <- Connection.use connection do
+          Session.pipeline do
+            (,,)
+              <$> Pipeline.statement () ok1
+              <*> Pipeline.statement () ok2
+              <*> Pipeline.statement () broken
+        error1 <- case result1 of
+          Left error1 -> pure error1
+          Right _ -> fail "First run unexpectedly succeeded"
+
+        -- Second run of the same pipeline: must fail with the SAME syntax error,
+        -- not "prepared statement already exists".
+        result2 <- Connection.use connection do
+          Session.pipeline do
+            (,,)
+              <$> Pipeline.statement () ok1
+              <*> Pipeline.statement () ok2
+              <*> Pipeline.statement () broken
+        error2 <- case result2 of
+          Left error2 -> pure error2
+          Right _ -> fail "Second run unexpectedly succeeded"
+        shouldBe error2 error1
+
+        -- Also, a standalone valid statement should still work afterwards.
+        result3 <- Connection.use connection do
+          Session.statement () ok1
+        case result3 of
+          Right val -> val `shouldBe` 1
+          Left err -> expectationFailure ("Unexpected error on standalone statement: " <> show err)
+
+    it "A pipeline with a broken statement in the middle can be retried without 'already exists' errors" \config -> do
+      Scripts.onPreparableConnection config \connection -> do
+        let ok1 = Statement.preparable "select 1" mempty (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+            broken = Statement.preparable "S" mempty Decoders.noResult
+            ok2 = Statement.preparable "select 2" mempty (Decoders.singleRow (Decoders.column (Decoders.nonNullable Decoders.int4)))
+        -- First run: pipeline with broken statement in the middle.
+        result1 <- Connection.use connection do
+          Session.pipeline do
+            (,,)
+              <$> Pipeline.statement () ok1
+              <*> Pipeline.statement () broken
+              <*> Pipeline.statement () ok2
+        shouldBe (isLeft result1) True
+
+        -- Second run of the same pipeline: must fail with the same syntax error.
+        result2 <- Connection.use connection do
+          Session.pipeline do
+            (,,)
+              <$> Pipeline.statement () ok1
+              <*> Pipeline.statement () broken
+              <*> Pipeline.statement () ok2
+        case result2 of
+          Left (Errors.StatementSessionError _ _ _ _ _ (Errors.ServerStatementError (Errors.ServerError "42601" _ _ _ _))) ->
+            pure ()
+          Left other ->
+            expectationFailure ("Unexpected error on second run: " <> show other)
+          Right _ ->
+            expectationFailure "Second run unexpectedly succeeded"
+
+        -- Standalone valid statements should still work afterwards.
+        result3 <- Connection.use connection do
+          Session.statement () ok1
+        case result3 of
+          Right val -> val `shouldBe` 1
+          Left err -> expectationFailure ("Unexpected error on standalone ok1: " <> show err)
+        result4 <- Connection.use connection do
+          Session.statement () ok2
+        case result4 of
+          Right val -> val `shouldBe` 2
+          Left err -> expectationFailure ("Unexpected error on standalone ok2: " <> show err)
