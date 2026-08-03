f6ed58dd46c415a394c94524f5d4fb9c13155b31
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-08-03 11:58:51 +0300

    Replace RowReader's StateT/ReaderT/ExceptT stack with a closure-based decoder

--- src/library/Hasql/Comms/RowReader.hs
+++ src/library/Hasql/Comms/RowReader.hs
@@ -32,62 +32,84 @@ data CellError
   | UnexpectedNullCellError
   deriving stock (Eq, Show)
 
+-- | Outcome of running a 'RowReader' starting from a given column: either it
+-- failed, or it produced a value along with the column position to resume
+-- decoding from.
+data Outcome a
+  = Failure Error
+  | Success a Pq.Column
+
+-- | A row reader is a closure threading the current column position
+-- explicitly through its arguments and return value, instead of through a
+-- monad transformer stack. Composing readers with '(<*>)' therefore costs
+-- nothing beyond the direct 'IO' calls each one performs.
 newtype RowReader a
-  = RowReader (StateT Pq.Column (ReaderT Env (ExceptT Error IO)) a)
-  deriving
-    (Functor, Applicative)
-    via (StateT Pq.Column (ReaderT Env (ExceptT Error IO)))
+  = RowReader (Pq.Result -> Pq.Row -> Pq.Column -> IO (Outcome a))
 
-data Env
-  = Env
-      Pq.Result
-      Pq.Row
+instance Functor RowReader where
+  {-# INLINE fmap #-}
+  fmap f (RowReader run) = RowReader \result row col -> do
+    outcome <- run result row col
+    pure case outcome of
+      Failure err -> Failure err
+      Success a col' -> Success (f a) col'
 
--- * Instances
+instance Applicative RowReader where
+  {-# INLINE pure #-}
+  pure a = RowReader \_ _ col -> pure (Success a col)
+  {-# INLINE (<*>) #-}
+  RowReader runF <*> RowReader runA = RowReader \result row col -> do
+    outcomeF <- runF result row col
+    case outcomeF of
+      Failure err -> pure (Failure err)
+      Success f col' -> do
+        outcomeA <- runA result row col'
+        pure case outcomeA of
+          Failure err -> Failure err
+          Success a col'' -> Success (f a) col''
 
 instance Filterable RowReader where
   {-# INLINE mapMaybe #-}
-  mapMaybe fn (RowReader run) =
-    RowReader do
-      result <- run
-      case fn result of
-        Just refined -> pure refined
-        Nothing -> throwError (RefinementError "Filtration failed")
+  mapMaybe fn (RowReader run) = RowReader \result row col -> do
+    outcome <- run result row col
+    pure case outcome of
+      Failure err -> Failure err
+      Success a col' -> case fn a of
+        Just refined -> Success refined col'
+        Nothing -> Failure (RefinementError "Filtration failed")
 
 -- * Functions
 
 {-# INLINE toHandler #-}
 toHandler :: RowReader a -> Pq.Result -> Pq.Row -> IO (Either Error a)
-toHandler (RowReader f) result row =
-  let env = Env result row
-   in runExceptT (runReaderT (evalStateT f 0) env)
+toHandler (RowReader run) result row = do
+  outcome <- run result row 0
+  pure case outcome of
+    Failure err -> Left err
+    Success a _ -> Right a
 
 -- |
 -- Next value, decoded using the provided value decoder.
 {-# INLINE column #-}
 column :: (Maybe a -> Maybe b) -> (ByteString -> Either Text a) -> RowReader b
-column processNullable valueDec = RowReader do
-  col <- get
-  Env result row <- ask
-  let colInt = Pq.colToInt col
-  put (succ col)
+column processNullable valueDec = RowReader \result row col -> do
+  rawMaybe <- {-# SCC "getvalue'" #-} Pq.getvalue' result row col
 
-  valueMaybe <- liftIO ({-# SCC "getvalue'" #-} Pq.getvalue' result row col)
+  decodedMaybe <- case rawMaybe of
+    Nothing -> pure (Right Nothing)
+    Just v -> case {-# SCC "decode" #-} valueDec v of
+      Left err -> do
+        oid <- Pq.oidToWord32 <$> Pq.ftype result col
+        pure (Left (CellError (Pq.colToInt col) oid (DecodingCellError err)))
+      Right decoded -> pure (Right (Just decoded))
 
-  valueMaybe <- case valueMaybe of
-    Nothing -> pure Nothing
-    Just v ->
-      case {-# SCC "decode" #-} valueDec v of
-        Left err -> do
-          oid <- Pq.oidToWord32 <$> liftIO (Pq.ftype result col)
-          throwError (CellError colInt oid (DecodingCellError err))
-        Right decoded -> pure (Just decoded)
-
-  case processNullable valueMaybe of
-    Nothing -> do
-      oid <- Pq.oidToWord32 <$> liftIO (Pq.ftype result col)
-      throwError (CellError colInt oid UnexpectedNullCellError)
-    Just decoded -> pure decoded
+  case decodedMaybe of
+    Left err -> pure (Failure err)
+    Right valueMaybe -> case processNullable valueMaybe of
+      Nothing -> do
+        oid <- Pq.oidToWord32 <$> Pq.ftype result col
+        pure (Failure (CellError (Pq.colToInt col) oid UnexpectedNullCellError))
+      Just decoded -> pure (Success decoded (succ col))
 
 -- |
 -- Next value, decoded using the provided value decoder.
